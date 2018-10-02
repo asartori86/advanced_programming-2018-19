384413dac2703e779672e4a3f61e7c7c2bf84d77
Alberto Sartori <alberto.sartori86@gmail.com>
2018-10-02 10:38:28 +0200

    std::swap

--- lectures/c++/01_intro/05_functions.cc
+++ lectures/c++/01_intro/05_functions.cc
@@ -1,24 +1,30 @@
 #include <iostream>
+#include <utility>  //std::swap
 
 void buggy_swap(int a, int b);
 void swap_c(int* a, int* b);
-void swap_cxx(int& a, int& b);  // can we do better?
+void swap_cxx(int& a, int& b);
 
 int main() {
   int a = 3;
   int b = {7};
   buggy_swap(a, b);
-  std::cout << "expected 7 3 --> " << a << " " << b << "\n";
+  std::cout << "buggy:\texpected 7 3 --> " << a << " " << b << "\n";
 
   a = 3;
   b = 7;
   swap_c(&a, &b);
-  std::cout << "expected 7 3 --> " << a << " " << b << "\n";
+  std::cout << "c:\texpected 7 3 --> " << a << " " << b << "\n";
 
   a = 3;
   b = 7;
   swap_cxx(a, b);
-  std::cout << "expected 7 3 --> " << a << " " << b << "\n";
+  std::cout << "cxx:\texpected 7 3 --> " << a << " " << b << "\n";
+
+  a = 3;
+  b = 7;
+  std::swap(a, b);
+  std::cout << "stl:\texpected 7 3 --> " << a << " " << b << "\n";
 }
 
 void buggy_swap(int a, int b) {
