5c3ff64010d24ba56fd930ee5912fa7a2c8b9616
Alberto Sartori <alberto.sartori86@gmail.com>
2018-12-06 02:35:17 +0100

    ex 01 python

---
+++ exercises/python/01_intro/exercises_01.py
@@ -0,0 +1,68 @@
+
+def is_palindrome(string_var):     
+    '''
+    returns True if string_var is palindrome, False otherwise
+    '''
+    ret = None
+    ########################
+    # code here
+    ########################
+    return ret 
+
+def histogram_letters(string_var):     
+    '''
+    count how many times a letter is present in the string_var
+    and returns a dict
+
+    '''
+    ret = None
+    ########################
+    # code here
+    ########################
+    return ret 
+
+def get_most_frequent(list_var):
+    '''
+    returns a tuple with the most frequent object in the list with the 
+    corresponing number
+    '''
+    ret = None
+    ########################
+    # code here
+    ########################
+    return ret
+
+def which_duplicates(list_var):
+    '''
+    return a dict with containing the repeted object and the number of repetitions
+    '''
+    ret = None
+    ########################
+    # code here
+    ########################
+    return ret
+
+
+def compute_factorial(int_val):
+    '''
+    returns factorial of a number
+    '''
+    ret = None
+    ########################
+    # code here
+    ########################
+    return ret
+
+
+def is_prime(int_val):
+    '''
+    returns True if int_val is prime, False otherwise
+    '''
+    ret = None
+    ########################
+    # code here
+    ########################
+    return ret
+
+        
+
