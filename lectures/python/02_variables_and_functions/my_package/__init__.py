561e7529048158fb8df70192f801d86952782d08
Alberto Sartori <alberto.sartori86@gmail.com>
2018-12-11 13:14:37 +0100

    Lecture 02 - Python contents: - modules and packages - more on function arguments - local, global, nonlocal variables - copy and deepcopy - classes

---
+++ lectures/python/02_variables_and_functions/my_package/__init__.py
@@ -0,0 +1,11 @@
+# it must be present
+
+# can be empty
+
+# or set the __all__ variable, which overrides *
+
+__all__=['foo_module','bar_module']
+#__all__=['foo_module']
+
+# if the __all__ variable contains all the modules
+# do not define it
